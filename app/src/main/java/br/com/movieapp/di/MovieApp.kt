2876ee019a1aa6b9e947697474c86d3ac19e570a
leonardocalderal <leonardo.calderal@picpay.com>
2024-03-23 11:39:31 -0300

    creating the dependency injection

---
+++ app/src/main/java/br/com/movieapp/di/MovieApp.kt
@@ -0,0 +1,17 @@
+package br.com.movieapp.di
+
+import android.app.Application
+import br.com.movieapp.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class MovieApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
