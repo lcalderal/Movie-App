fc2336a83447baf2357c2e9db6126dd9bb853ec0
leonardocalderal <leonardo.calderal@picpay.com>
2024-03-23 11:58:54 -0300

    Creating the NetworkModules

---
+++ app/src/main/java/br/com/movieapp/framework/di/NetworkModule.kt
@@ -0,0 +1,66 @@
+package br.com.movieapp.framework.di
+
+import br.com.movieapp.BuildConfig
+import br.com.movieapp.framework.data.remote.ParamsInterceptor
+import br.com.movieapp.framework.data.remote.service.MovieService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val TIMEOUT_SECOND = 15L
+
+    @Provides
+    fun provideParamsInterceptor(): ParamsInterceptor {
+        return ParamsInterceptor()
+    }
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        paramsInterceptor: ParamsInterceptor,
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(paramsInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideMovieService(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): MovieService {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+            .create(MovieService::class.java)
+    }
+}
